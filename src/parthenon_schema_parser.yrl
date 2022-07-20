1a932c3d5111ab1dc776bdfc03e23ecb3cc0efb8
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2022-07-20 18:53:55 -0400

    Convert `null` values to `undefined`

--- src/parthenon_schema_parser.yrl
+++ src/parthenon_schema_parser.yrl
@@ -11,12 +11,15 @@ struct_root -> 'struct' struct_list : maps:from_list('$2').
 struct_list -> '<' struct_list_elements '>' : '$2'.
 
 struct_list_elements ->  struct_element : [ '$1' ].
+
 struct_list_elements -> struct_element ',' struct_list_elements : [ '$1' | '$3' ].
 
 struct_element -> word ':' encoder : mapping('$1', '$3').
 
 encoder -> encoding : create_encoder('$1').
+
 encoder -> 'array' '<' encoder '>' : create_encoder({list, '$3'}).
+
 encoder -> struct_root : '$1'.
 
 Erlang code.
@@ -28,13 +31,21 @@ extract_identifier({word, _Line, Name}) ->
     Name.
 
 create_encoder({encoding, _Line, Encoding}) ->
-    to_encoder(Encoding);
+    Encoder = to_encoder(Encoding),
+    with_null_as_undefined(Encoder);
 create_encoder({list, Encoder}) when is_map(Encoder) ->
     {map_array, Encoder};
 create_encoder({list, Encoder}) ->
-    fun (List) -> lists:map(Encoder, List) end;
+    ListEncoder = fun(List) -> lists:map(Encoder, List) end,
+    with_null_as_undefined(ListEncoder);
 create_encoder(Unknown) ->
     throw({unknown_encoding, Unknown}).
+
+with_null_as_undefined(F) ->
+    fun
+        (<<"null">>) -> undefined;
+        (Other) -> F(Other)
+    end.
 
 to_encoder(int) ->
     fun binary_to_integer/1;
--- test/parthenon_schema_SUITE.erl
+++ test/parthenon_schema_SUITE.erl
@@ -16,7 +16,8 @@ groups() ->
             can_parse_nested_structs,
             return_error_on_invalid_encoding,
             contain_correct_encoder,
-            can_parse_complex_schema
+            can_parse_complex_schema,
+            return_undefined_on_null_values
         ]}
     ].
 
@@ -88,6 +89,30 @@ contain_correct_encoder(_Config) ->
 
 can_parse_complex_schema(Config) ->
     ?assertMatch({ok, _}, parthenon_schema:create(?config(raw_schema, Config))).
+
+return_undefined_on_null_values(_Config) ->
+    {ok, Schema} = parthenon_schema:create(
+        <<
+            "struct<boolean_: boolean, integer: int, double_: double, big_integer: bigint, string_: string,"
+            "integer_list: array<int>, boolean_list: array<boolean>, double_list: array<double>, big_integer_list: array<bigint>,"
+            "string_list: array<string>>"
+        >>
+    ),
+
+    ?assertEqual(undefined, apply_encoder(integer, <<"null">>, Schema)),
+    ?assertEqual(undefined, apply_encoder(boolean_, <<"null">>, Schema)),
+    ?assertEqual(undefined, apply_encoder(double_, <<"null">>, Schema)),
+    ?assertEqual(undefined, apply_encoder(big_integer, <<"null">>, Schema)),
+    ?assertEqual(undefined, apply_encoder(string_, <<"null">>, Schema)),
+    ?assertEqual(undefined, apply_encoder(integer_list, <<"null">>, Schema)),
+    ?assertEqual([undefined], apply_encoder(integer_list, [<<"null">>], Schema)),
+    ?assertEqual(
+        [undefined, undefined],
+        apply_encoder(boolean_list, [<<"null">>, <<"null">>], Schema)
+    ),
+    ?assertEqual([undefined], apply_encoder(double_list, [<<"null">>], Schema)),
+    ?assertEqual([undefined], apply_encoder(big_integer_list, [<<"null">>], Schema)),
+    ?assertEqual([undefined], apply_encoder(string_list, [<<"null">>], Schema)).
 
 %%%===================================================================
 %%% Internal functions
