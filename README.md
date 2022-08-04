6b87c39945b25192da9caa7f65f14b754019ce7b
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2022-08-04 16:15:42 -0400

    Improve documentation (#12)

---
+++ README.md
@@ -0,0 +1,54 @@
+# parthenon
+
+[![Build Status](https://github.com/AntoineGagne/parthenon/actions/workflows/erlang.yml/badge.svg)](https://github.com/AntoineGagne/parthenon/actions)
+[![Coverage](https://coveralls.io/repos/AntoineGagne/parthenon/badge.png?branch=master)](https://coveralls.io/r/AntoineGagne/parthenon?branch=master)
+[![Hex Pm](http://img.shields.io/hexpm/v/parthenon.svg?style=flat)](https://hex.pm/packages/parthenon)
+[![Docs](https://img.shields.io/badge/hex-docs-green.svg?style=flat)](https://hexdocs.pm/parthenon)
+[![Erlang Versions](https://img.shields.io/badge/Supported%20Erlang%2FOTP-21.0%20to%2025.0-blue)](http://www.erlang.org)
+
+`parthenon` is an OTP application that parses AWS Athena structures into Erlang terms.
+
+## Usage
+
+Inside `rebar.config`:
+
+```erl
+%% ...
+{deps, [
+    %% ...
+    parthenon
+]}.
+```
+
+Inside ``.app.src``:
+
+```erl
+%% ...
+{applications, [
+    %% ...
+    parthenon
+]},
+%% ...
+```
+
+Inside the code:
+
+```erl
+parthenon:add_schema(athena_structure, <<"struct<a: int, b: int>">>).
+parthenon:decode(athena_structure, <<"{a=123, b=456}">>).
+
+{ok, Binary} = file:read_file("/path/to/athena-structure-schema").
+parthenon:add_schema(athena_structure, Binary).
+{ok, RawStructure} = file:read_file("/path/to/raw-athena-structure").
+parthenon:decode(athena_structure, RawStructure).
+```
+
+## Development
+
+### Running all the tests and linters
+
+You can run all the tests and linters with the ``rebar3`` alias:
+
+```sh
+rebar3 check
+```
--- src/parthenon.erl
+++ src/parthenon.erl
@@ -1,3 +1,6 @@
+%% @author Antoine Gagné <gagnantoine@gmail.com>
+%% @copyright 2022 Antoine Gagné
+%% @doc Parse Athena structures and schemas.
 -module(parthenon).
 
 -export([
@@ -11,15 +14,56 @@
 %%%===================================================================
 
 -spec add_schema(SchemaName :: atom(), RawSchema :: binary() | string()) -> ok | {error, term()}.
+%% @doc
+%% Parse the provided schema and register it in the list of available schemas
+%% as the specified name.
+%% @end
 add_schema(SchemaName, RawSchema) ->
     parthenon_schema_server:add_schema(SchemaName, RawSchema).
 
 -spec decode(SchemaName :: atom(), Binary :: binary()) -> {ok, term()} | {error, term()}.
+%% @doc
+%% Parse the raw Athena structure with the specified schema.
+%% @end
 decode(SchemaName, Binary) ->
     parthenon_decode:try_decode(SchemaName, Binary).
 
 -spec decode(SchemaName :: atom(), Binary :: binary(), Options :: [parthenon_decode:option()]) ->
     {ok, term()} | {error, term()}.
+%% @doc
+%% Parse the raw Athena structure with the specified schema and apply the
+%% specified options.
+%%
+%% The supported options are:
+%%
+%% <ul>
+%%   <li>`object_format': Specify the format of the objects.
+%%      <ul>
+%%        <li>`maps' (default): return objects as maps</li>
+%%        <li>`proplists': return objects as proplists</li>
+%%        <li>`tuple' will return objects in the `{[{<key>, <value>}]}' format</li>
+%%      </ul>
+%%   </li>
+%%   <li>`key_format': Specify the format of the keys.
+%%     <ul>
+%%       <li>`atom': return the keys as atoms</li>
+%%       <li>`binary' will return the keys as binary.</li>
+%%       <li>`existing_atom' (default): return the keys as atoms if they
+%%       already exists or return them into binary if they do not</li>
+%%     </ul>
+%%  </li>
+%% </ul>
+%%
+%% Example usage:
+%%
+%% ```
+%% %% Register the `point' schema into the registry
+%% ok = parthenon:add_schema(point, <<"struct<x: int, y: int, z: int>">>).
+%%
+%% %% Decode the `point' structure into a map with binary keys
+%% {ok, #{<<"x">> := 3, <<"y">> := 2, <<"z">> := 4}} = parthenon:decode(point, <<"{x=3, y=2, z=4}">>, [{key_format, binary}, {object_format, maps}]).
+%% '''
+%% @end
 decode(SchemaName, Binary, Options) ->
     parthenon_decode:try_decode(SchemaName, Binary, Options).
 
